229ab17e1b0a9405b3aedfe0a1d8fd2c6be44a52
heidar alavi <alavi.h@gmail.com>
2024-11-19 23:04:59 +0330

    PCA and Kmeans tested

--- code/kmean-oxy.py
+++ code/kmean-oxy.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import seaborn as sns
 from datetime import datetime
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 
 import os
 
@@ -16,17 +19,41 @@ df = pd.read_csv(f"{working_dir}/data/Oxygen_Plant_24Days.csv")
 df.drop(labels='time',axis=1,inplace=True)
 #print(df.info())
 
+scale = MinMaxScaler()
+normal_data = scale.fit_transform(df)
+#print(normal_data)
 
-km_df = df.filter(['AI1 Product gaseous (liquid) oxygen purity','Analys Argon (AI7) Product argon purity (O2 content)','AI4 Oxygen content in waste nitrogen'],axis=1) #,'AI4 Oxygen content in waste nitrogen'
-#km_df = km_df.reindex(['AI4 Oxygen content in waste nitrogen','AI1 Product gaseous (liquid) oxygen purity'], axis=1)
-print(km_df.head())
-km_analysis = KMeans(n_clusters=8,init='k-means++', n_init=10)
-clust_lable_dict = {0:'l1',1:'l2',2:'l3',3:'l4',4:'l5',5:'l6',6:'l7',7:'l8'}
-km_analysis.fit(km_df)
-labels = [clust_lable_dict[i] for i in km_analysis.labels_]
+pca = PCA(n_components=2)
+pca_output = pca.fit_transform(normal_data)
+print(pca.explained_variance_ratio_)
+
+km = KMeans(n_clusters= 4,random_state= 0)
+km.fit(pca_output)
+clust_lable_dict = {0:'Good',1:'out',2:'Bad',3:'Very Good',4:'l5',5:'l6',6:'l7',7:'l8'}
+labels = [clust_lable_dict[i] for i in km.labels_]
+
+#//Air turbine outlet pressure VS products
+fig = plt.figure(figsize=(15,11),dpi=300)
+fig.suptitle('Tubine Outlet Pressure VS Air Production', fontsize=18,fontweight='bold')
+ax1 = fig.subplots(1,1)
+ax1.set_xlabel('bar', fontsize=16,fontweight='bold')
+sns.scatterplot(
+    #data=df,
+    x=pca_output[:,0],
+    #y='AI4 Oxygen content in waste nitrogen',
+    y=pca_output[:,1],
+    hue=labels,
+    alpha = 0.9,
+)
+fig.tight_layout()
+#plt.savefig(f'{working_dir}/temp/temp.jpg')
+#plt.show()
+fig.clear()
+
+
+
 df['kmeans_labels'] = labels
-#print(df.head())
-
+print(df.head())
 
 #//Air turbine outlet pressure VS products
 fig = plt.figure(figsize=(15,11),dpi=300)
@@ -35,14 +62,14 @@ ax1 = fig.subplots(1,1)
 ax1.set_xlabel('bar', fontsize=16,fontweight='bold')
 sns.scatterplot(
     data=df,
-    #x=df.index,
-    x='AI4 Oxygen content in waste nitrogen',
+    x=df.index,
+    #y='AI4 Oxygen content in waste nitrogen',
     y='AI1 Product gaseous (liquid) oxygen purity',
     hue='kmeans_labels',
     alpha = 0.9,
 )
 fig.tight_layout()
-plt.savefig(f'{working_dir}/temp/temp.jpg')
+#plt.savefig(f'{working_dir}/temp/temp.jpg')
 #plt.show()
 fig.clear()
 
@@ -81,6 +108,6 @@ plt.annotate('27793 RPM', xy =(27787, 0.0023),rotation = 90,ha='center', fontsiz
 #plt.axvline(210, c='red')
 #plt.annotate('224', xy =(224, 0.009),rotation = 90,ha='center', fontsize=16) 
 fig.tight_layout()
-#plt.savefig(f'{working_dir}/temp/temp.jpg')
+plt.savefig(f'{working_dir}/temp/temp.jpg')
 #plt.show()
 fig.clear()
