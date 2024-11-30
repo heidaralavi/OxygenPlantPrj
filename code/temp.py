5f96dfad01d41caa0cc646eedfce811365ac7366
heidar alavi <alavi.h@gmail.com>
2024-11-30 15:29:26 +0330

    some work on temp

--- code/temp.py
+++ code/temp.py
@@ -1,4 +1,18 @@
 import os
-#working_dir = os.getcwd()
-working_dir ='..'  # Use on Jupyter Notebook
+import numpy as np
+import pandas as pd
+working_dir = os.getcwd()
+#working_dir ='..'  # Use on Jupyter Notebook
 print(os.getcwd())
+#df = pd.read_csv(f"{working_dir}/data/Oxygen_Plant_24Days.dat" , delimiter='\r\n')
+#print(df)
+#with open(f"{working_dir}/data/Oxygen_Plant_24Days.dat", 'rb') as f:
+#  text = f.read(16).decode(encoding='utf-8' ,errors='ignore')
+  
+#pd.read_
+#print(text)
+
+#x = np.fromfile(f"{working_dir}/data/Oxygen_Plant_24Days.dat", dtype='uint8')
+#print (x.max())
+#for item in x:
+#  print(chr(item))
